11820d5017ad4ea48c99cb363c80ce3a38c85493
Yash Jindal <yashjindal069@gmail.com>
2023-06-23 01:12:23 +0530

    made changes

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,27 +2,27 @@ import React from "react";
 
 const Login = () => {
   return (
-    <div className="flex flex-col sm:flex-row justify-between items-center w-full ">
-      <div className="flex h-[80px] sm:min-h-screen justify-center items-center w-full sm:w-[65%] bg-black">
+    <div className="flex flex-col sm:flex-row justify-between bg-[#F5F5F5] items-center w-full ">
+      <div className="flex h-[60px] sm:h-[80px] sm:min-h-screen justify-center items-center w-full sm:w-[65%] bg-black">
         <h1 className="text-white font-bold text-[2rem] md:text-[3rem] lg:text-[4.5rem]">
           Board.
         </h1>
       </div>
-      <div className="flex flex-col min-h-screen gap-6 bg-[#F5F5F5] justify-center w-full items-center">
+      <div className="flex flex-col mt-14 w-full gap-6 justify-center items-center">
         <div className="flex flex-col justify-center gap-4">
-          <div className="flex flex-col justify-between items-start">
-            <h2 className="text-[24px] sm:text-[36px] text-black font-bold leading-[43.88px]">
+          <div className="flex flex-col justify-between items-start px-6">
+            <h2 className="text-[18px] sm:text-[36px] text-black font-bold sm:leading-[43.88px]">
               Sign In
             </h2>
-            <p className="text-[16px] font-lato leading-[19.2px] text-black">
+            <p className="text-[16px] font-lato sm:leading-[19.2px] text-black">
               Sign in to your account
             </p>
           </div>
           <div className="flex justify-center items-center gap-[10px]">
-            <button className="border-[2px] rounded-lg bg-white h-[40px] px-6 border-black">
+            <button className="text-[12px] text-[#858585] w-[160px] sm:w-[180px] border-[2px] rounded-lg bg-white h-[40px] px-6 border-black">
               Sign in with Google
             </button>
-            <button className="border-[2px] rounded-lg bg-white h-[40px] px-6 border-black">
+            <button className="text-[12px] text-[#858585] w-[150px] sm:w-[180px] border-[2px] rounded-lg bg-white h-[40px] px-6 border-black">
               Sign in with Apple
             </button>
           </div>
@@ -33,7 +33,7 @@ const Login = () => {
                   Email Address
                 </h1>
                 <input
-                  className="bg-[#F5F5F5] w-[325px] h-[40px] px-6 rounded-xl"
+                  className="bg-[#F5F5F5] w-[280px] sm:w-[325px] h-[40px] px-6 rounded-xl"
                   placeholder="Email..."
                 />
               </div>
@@ -42,12 +42,12 @@ const Login = () => {
                   Password
                 </h1>
                 <input
-                  className="bg-[#F5F5F5] w-[325px] h-[40px] px-6 rounded-xl"
+                  className="bg-[#F5F5F5] w-[280px] sm:w-[325px] h-[40px] px-6 rounded-xl"
                   placeholder="Password..."
                 />
               </div>
               <p className="text-[#346BD4]">Forgot password?</p>
-              <button className="bg-black rounded-xl w-[325px] h-[40px] font-bold text-white">
+              <button className="bg-black rounded-xl w-[280px] sm:w-[325px] h-[40px] font-bold text-white">
                 Sign In
               </button>
             </form>
